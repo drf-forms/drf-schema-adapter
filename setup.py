75380be01adde5bd55063e777c8c4ebfebd6ed4d
Emmanuelle Delescolle <emma@levit.be>
2016-12-31 12:16:23 +0100

    :bug: running setup locally shouldn't require pypandoc

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 import os
 from setuptools import setup
 
-import pypandoc
+try:
+    import pypandoc
 
-README = pypandoc.convert_file('README.md', 'rst')
+
+    README = pypandoc.convert_file('README.md', 'rst')
+except ImportError:
+    with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+        README = readme.read()
+
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
