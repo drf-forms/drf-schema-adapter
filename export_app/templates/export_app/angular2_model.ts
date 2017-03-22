a78c73eb654e89b2c033500087acf54656b200a4
Emmanuelle Delescolle <emma@levit.be>
2017-03-22 15:41:31 +0100

    :white_check_mark: update test env settings

--- export_app/templates/export_app/angular2_model.ts
+++ export_app/templates/export_app/angular2_model.ts
@@ -1,4 +1,4 @@
-import {{application_name|title}}{{model_name|title}}Base from './{{model_name}}_base';
+import {{application_name|title}}{{model_name|title}}Base from './{{model_name}}.base';
 
 export default class {{application_name|title}}{{model_name|title}} extends {{application_name|title}}{{model_name|title}}Base {
 
--- export_app/templates/export_app/angular2_service.ts
+++ export_app/templates/export_app/angular2_service.ts
@@ -1,4 +1,4 @@
-import {{application_name|title}}{{model_name|title}}ServiceBase from './{{model_name}}_service_base';
+import {{application_name|title}}{{model_name|title}}ServiceBase from './{{model_name}}.service.base';
 
 export default class {{application_name|title}}{{model_name|title}}Service extends {{application_name|title}}{{model_name|title}}ServiceBase {
 
