462f1f11ea7f9fb5c9e317090d9ab5ba0a8e5e96
Nicolas PASCAL <np.pascal@gmail.com>
2018-04-11 16:53:08 +0200

    Restore the code that also catches ImportError

--- setup.py
+++ setup.py
@@ -5,9 +5,13 @@ try:
     import pypandoc
 
     README = pypandoc.convert_file('README.md', 'rst')
+except ImportError:
+    with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
 except OSError:
     # pandoc is not installed, fallback to using raw contents
     README = open('README.md').read()
+
 
 
 # allow setup.py to be run from any path
