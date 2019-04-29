d310babddcb522d6caee7920e808335b8678b096
Jan Rydzewski <flegmer@gmail.com>
2019-04-29 11:30:33 +0200

    Revert changes in settings.py

--- settings.py
+++ settings.py
@@ -43,11 +43,12 @@ INSTALLED_APPS = (
     'export_app',
 )
 
-MIDDLEWARE = (
+MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
