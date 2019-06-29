352b091ee20a48605401cfa56676b317ed29db87
Emmanuelle Delescolle <emma@levit.be>
2019-06-29 10:02:39 +0200

    :bug: fix settings after merge

--- settings.py
+++ settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = (
     'export_app',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
