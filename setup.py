9a270803e1d2010a21e6341672644bf8cfd790ca
Emmanuelle Delescolle <emma@levit.be>
2023-02-21 14:05:24 +0100

    :bookmark: 3.0.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='drf-schema-adapter',
-    version='3.0.2',
+    version='3.0.3',
     packages=['drf_auto_endpoint', 'export_app', 'export_app.management',
               'export_app.management.commands'],
     include_package_data=True,
